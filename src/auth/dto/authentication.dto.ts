9d6697a0ac13fa01c712c86d0bee15956f57faa9
Christothea <49829786+Christothea@users.noreply.github.com>
2020-10-06 00:27:03 +0300

    Added bearer auth on swagger

--- src/auth/dto/authentication.dto.ts
+++ src/auth/dto/authentication.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class AuthenticateDto {
+  @ApiProperty({
+    name: 'username',
+    type: String,
+  })
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    name: 'password',
+    type: String,
+  })
   @IsNotEmpty()
   password: string;
 }
--- src/auth/dto/token.dto.ts
+++ src/auth/dto/token.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TokenDto {
+  @ApiProperty({
+    name: 'expiresIn',
+    type: Date,
+  })
   expiresIn: Date;
+
+  @ApiProperty({
+    name: 'authToken',
+    type: String,
+  })
   authToken: string;
 
   constructor(partial: Partial<TokenDto> = {}) {
